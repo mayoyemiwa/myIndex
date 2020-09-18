9c65fdb17c943c52efc1a393e70b1bb97835d199
mayoyemiwa <mayoyemiwa@gmail.com>
2020-09-18 02:02:08 +0100

    changes

--- index.js
+++ index.js
@@ -24,27 +24,45 @@ app.get('/api/courses/:id', (req, res)=>{
       return res.status(404).send(`The course with the given ID ${req.params.id} was not found`);
 });
 
-app.post('/api/courses', (req, res,)=>{
-    const Schema = {
+app.post('/api/courses', async (req, res,)=>{
+    const Schema = Joi.object({
         name: Joi.string().min(3).required() 
-      }; 
-     
+      })
+      .with('name')
+  
+  
+  schema.validate({ name: 'abc'});
+  // -> { value: { username: 'abc', birth_year: 1994 } }
+  
+  schema.validate({});
+  // -> { value: {}, error: '"username" is required' }
+  
+  // Also -
+  
+  try {
+      const value = await schema.validateAsync({ name: 'abc'});
+  
+     if(value){
+        const course = {
+            id:courses.length + 1,
+            name:req.body.name}
+    
+              courses.push(course);
+        res.send(course);
+  }
+}
+  catch (err) { 
+  if(error){}
+  res.status(422).send(result.error);
+            return ;
+}
       const result = Joi.validate(req.body.name, Schema); 
       const { value, error } = result; 
-      if(error){ 
-        res.status(422).send(result.error);
-            return ;
+      
 }  
-else if(value){
-    const course = {
-        id:courses.length + 1,
-        name:req.body.name}
-
-          courses.push(course);
-    res.send(course);
-}   
+  
           
-    });
+    );
 
 const port = process.env.PORT || 3000;
 app.listen(port, ()=>{console.log(`Listening on port ${port}...`)});
